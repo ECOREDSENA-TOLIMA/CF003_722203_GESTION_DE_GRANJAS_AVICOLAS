cff8742561a8f57bee815f0dd381feea35d97692
Francisco Vasquez <fjvasquez8@misena.edu.co>
2023-09-06 08:26:36 -0500

    Avance
    Avance

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -15,7 +15,7 @@
     
     figure.mt-5.mb-5(data-aos="fade-right")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/D8fBbV1Ekes" title="Estrategias y análisis de mercados" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)      
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/njQRxlRlBh8" title="Estrategias y análisis de mercados" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)      
   
 
 </template>
